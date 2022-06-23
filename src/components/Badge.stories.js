dd026e39466286c188cc5b6fe761abf36e65bea9
Lucas <lucas@owlint.fr>
2022-06-23 17:32:48 +0200

    feat(badge): component implementation

---
+++ src/components/Badge.stories.js
@@ -0,0 +1,97 @@
+import Badge from './Badge.vue'
+import { COLORS } from '../enums/colors'
+import { SIZES } from '../enums/sizes'
+
+export default {
+  component: Badge,
+  //👇 Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+  title: 'Components/Badge',
+  decorators: [
+    () => ({
+      template:
+        '<div class="flex items-center gap-4 flex-wrap "><story/></div>',
+    }),
+  ],
+  argTypes: {
+    color: {
+      options: COLORS,
+      control: { type: 'select' },
+    },
+    size: {
+      options: SIZES,
+      control: { type: 'select' },
+    },
+    tile: {
+      type: { name: 'boolean', required: false },
+      options: [true, false],
+      control: { type: 'inline-radio' },
+    },
+    text: {
+      type: { name: 'string', required: true },
+      description: 'Contenu du composant',
+    },
+  },
+}
+
+const defaultArgs = {
+  text: 'Badge',
+}
+
+const Template = (args) => ({
+  components: { Badge },
+  setup() {
+    //👇 The args will now be passed down to the template
+    return { args }
+  },
+  template: `<Badge v-bind="args" > ${args.text} </Badge>`,
+})
+
+export const Default = Template.bind({})
+Default.args = defaultArgs
+
+export const Tile = (args) => ({
+  components: { Badge },
+  setup() {
+    return { args }
+  },
+  template: `
+    <Badge > Badge Default </Badge>
+    <Badge tile > Badge Tile </Badge>
+  `,
+})
+export const Icon = (args) => ({
+  components: { Badge },
+  setup() {
+    return { args }
+  },
+  template: `
+    <Badge> Badge Default </Badge>
+    <Badge> 
+        <div class="flex items-center gap-2">
+                <font-awesome-icon  :icon="['fad', 'burger-cheese']" />
+                <span> Burger </span>
+        </div>
+    </Badge>
+  `,
+})
+
+export const Sizes = (args) => ({
+  components: { Badge },
+  setup() {
+    return { args }
+  },
+  template: `<Badge v-for="size in ['xs', 'sm', 'md', 'lg', 'xl']" :key="size" :size="size" >
+                Badge {{size}}
+            </Badge>`,
+})
+export const Colors = (args) => ({
+  components: { Badge },
+  setup() {
+    const colors = COLORS
+    return { args, colors }
+  },
+  template: `<Badge v-for="color in colors" :key="color" :color="color" >
+                Badge {{color}}
+            </Badge>`,
+})
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,50 +1,100 @@
 export const getIconStyle = (color) => {
   switch (color) {
     case 'indigo':
-      return 'text-indigo-500 bg-indigo-100 dark:bg-slate-900 dark:text-indigo-400/80'
+      return 'text-indigo-500 bg-indigo-100 dark:bg-nosferatu-800 dark:text-indigo-500/80'
     case 'slate':
-      return 'text-slate-500 bg-slate-100 dark:bg-slate-900 dark:text-slate-400/80'
+      return 'text-slate-500 bg-slate-100 dark:bg-nosferatu-800 dark:text-slate-500/80'
     case 'gray':
-      return 'text-gray-500 bg-gray-100 dark:bg-gray-900 dark:text-gray-400/80'
+      return 'text-gray-500 bg-gray-100 dark:bg-nosferatu-800 dark:text-gray-500/80'
     case 'zinc':
-      return 'text-zinc-500 bg-zinc-100 dark:bg-slate-900 dark:text-zinc-400/80'
+      return 'text-zinc-500 bg-zinc-100 dark:bg-nosferatu-800 dark:text-zinc-500/80'
     case 'neutral':
-      return 'text-neutral-500 bg-neutral-100 dark:bg-slate-900 dark:text-neutral-400/80'
+      return 'text-neutral-500 bg-neutral-100 dark:bg-nosferatu-800 dark:text-neutral-500/80'
     case 'stone':
-      return 'text-stone-500 bg-stone-100 dark:bg-slate-900 dark:text-stone-400/80'
+      return 'text-stone-500 bg-stone-100 dark:bg-nosferatu-800 dark:text-stone-500/80'
     case 'red':
-      return 'text-red-500 bg-red-100 dark:bg-slate-900 dark:text-red-400/80'
+      return 'text-red-500 bg-red-100 dark:bg-nosferatu-800 dark:text-red-500/80'
     case 'orange':
-      return 'text-orange-500 bg-orange-100 dark:bg-slate-900 dark:text-orange-400/80'
+      return 'text-orange-500 bg-orange-100 dark:bg-nosferatu-800 dark:text-orange-500/80'
     case 'amber':
-      return 'text-amber-500 bg-amber-100 dark:bg-slate-900 dark:text-amber-400/80'
+      return 'text-amber-500 bg-amber-100 dark:bg-nosferatu-800 dark:text-amber-500/80'
     case 'yellow':
-      return 'text-yellow-500 bg-yellow-100 dark:bg-slate-900 dark:text-yellow-400/80'
+      return 'text-yellow-500 bg-yellow-100 dark:bg-nosferatu-800 dark:text-yellow-500/80'
     case 'lime':
-      return 'text-lime-500 bg-lime-100 dark:bg-slate-900 dark:text-lime-400/80'
+      return 'text-lime-500 bg-lime-100 dark:bg-nosferatu-800 dark:text-lime-500/80'
     case 'green':
-      return 'text-green-500 bg-green-100 dark:bg-slate-900 dark:text-green-400/80'
+      return 'text-green-500 bg-green-100 dark:bg-nosferatu-800 dark:text-green-500/80'
     case 'emerald':
-      return 'text-emerald-500 bg-emerald-100 dark:bg-slate-900 dark:text-emerald-400/80'
+      return 'text-emerald-500 bg-emerald-100 dark:bg-nosferatu-800 dark:text-emerald-500/80'
     case 'teal':
-      return 'text-teal-500 bg-teal-100 dark:bg-slate-900 dark:text-teal-400/80'
+      return 'text-teal-500 bg-teal-100 dark:bg-nosferatu-800 dark:text-teal-500/80'
     case 'cyan':
-      return 'text-cyan-500 bg-cyan-100 dark:bg-slate-900 dark:text-cyan-400/80'
+      return 'text-cyan-500 bg-cyan-100 dark:bg-nosferatu-800 dark:text-cyan-500/80'
     case 'sky':
-      return 'text-sky-500 bg-sky-100 dark:bg-slate-900 dark:text-sky-400/80'
+      return 'text-sky-500 bg-sky-100 dark:bg-nosferatu-800 dark:text-sky-500/80'
     case 'blue':
-      return 'text-blue-500 bg-blue-100 dark:bg-slate-900 dark:text-blue-400/80'
+      return 'text-blue-500 bg-blue-100 dark:bg-nosferatu-800 dark:text-blue-500/80'
     case 'violet':
-      return 'text-violet-500 bg-violet-100 dark:bg-slate-900 dark:text-violet-400/80'
+      return 'text-violet-500 bg-violet-100 dark:bg-nosferatu-800 dark:text-violet-500/80'
     case 'purple':
-      return 'text-purple-500 bg-purple-100 dark:bg-slate-900 dark:text-purple-400/80'
+      return 'text-purple-500 bg-purple-100 dark:bg-nosferatu-800 dark:text-purple-500/80'
     case 'fuchsia':
-      return 'text-fuchsia-500 bg-fuchsia-100 dark:bg-slate-900 dark:text-fuchsia-400/80'
+      return 'text-fuchsia-500 bg-fuchsia-100 dark:bg-nosferatu-800 dark:text-fuchsia-500/80'
     case 'pink':
-      return 'text-pink-500 bg-pink-100 dark:bg-slate-900 dark:text-pink-400/80'
+      return 'text-pink-500 bg-pink-100 dark:bg-nosferatu-800 dark:text-pink-500/80'
     case 'rose':
-      return 'text-rose-500 bg-rose-100 dark:bg-slate-900 dark:text-rose-400/80'
+      return 'text-rose-500 bg-rose-100 dark:bg-nosferatu-800 dark:text-rose-500/80'
     default:
-      return 'text-indigo-500 bg-indigo-100 dark:bg-slate-900 dark:text-indigo-400/80'
+      return 'text-indigo-500 bg-indigo-100 dark:bg-nosferatu-800 dark:text-indigo-500/80'
+  }
+}
+export const getBadgeStyle = (color) => {
+  switch (color) {
+    case 'indigo':
+      return 'text-indigo-900 bg-indigo-100 dark:bg-nosferatu-900 dark:text-dracula-400'
+    case 'slate':
+      return 'text-salte-900 bg-slate-100 dark:bg-nosferatu-900 dark:text-aro-400'
+    case 'gray':
+      return 'text-gray-900 bg-gray-100 dark:bg-nosferatu-900 dark:text-gray-400'
+    case 'zinc':
+      return 'text-zinc-900 bg-zinc-100 dark:bg-nosferatu-900 dark:text-zinc-400/80'
+    case 'neutral':
+      return 'text-neutral-900 bg-neutral-100 dark:bg-nosferatu-900 dark:text-neutral-400/80'
+    case 'stone':
+      return 'text-stone-900 bg-stone-100 dark:bg-nosferatu-900 dark:text-stone-400/80'
+    case 'red':
+      return 'text-red-900 bg-red-100 dark:bg-nosferatu-900 dark:text-marcelin-400'
+    case 'orange':
+      return 'text-orange-900 bg-orange-100 dark:bg-nosferatu-900 dark:text-morbius-400'
+    case 'amber':
+      return 'text-amber-900 bg-amber-100 dark:bg-nosferatu-900 dark:text-amber-400/80'
+    case 'yellow':
+      return 'text-yellow-900 bg-yellow-100 dark:bg-nosferatu-900 dark:text-lincoln-400'
+    case 'lime':
+      return 'text-lime-900 bg-lime-100 dark:bg-nosferatu-900 dark:text-lime-400/80'
+    case 'green':
+      return 'text-green-900 bg-green-100 dark:bg-nosferatu-900 dark:text-blade-400'
+    case 'emerald':
+      return 'text-emerald-900 bg-emerald-100 dark:bg-nosferatu-900 dark:text-emerald-400/80'
+    case 'teal':
+      return 'text-teal-900 bg-teal-100 dark:bg-nosferatu-900 dark:text-teal-400/80'
+    case 'cyan':
+      return 'text-cyan-900 bg-cyan-100 dark:bg-nosferatu-900 dark:text-cyan-400/80'
+    case 'sky':
+      return 'text-sky-900 bg-sky-100 dark:bg-nosferatu-900 dark:text-sky-400/80'
+    case 'blue':
+      return 'text-cyan-900 bg-cyan-100 dark:bg-nosferatu-900 dark:text-vanHelsing-400'
+    case 'violet':
+      return 'text-violet-900 bg-violet-100 dark:bg-nosferatu-900 dark:text-violet-400/80'
+    case 'purple':
+      return 'text-purple-900 bg-purple-100 dark:bg-nosferatu-900 dark:text-buffy-400'
+    case 'fuchsia':
+      return 'text-fuchsia-900 bg-fuchsia-100 dark:bg-nosferatu-900 dark:text-fuchsia-400/80'
+    case 'pink':
+      return 'text-pink-900 bg-pink-100 dark:bg-nosferatu-900 dark:text-pink-400/80'
+    case 'rose':
+      return 'text-rose-900 bg-rose-100 dark:bg-nosferatu-900 dark:text-rose-400/80'
+    default:
+      return 'text-indigo-900 bg-indigo-100 dark:bg-nosferatu-900 dark:text-dracula-400'
   }
 }

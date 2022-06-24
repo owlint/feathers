34b4c3a153d68f4f3d89f6809f7c6b0a378b5574
Lucas <lucas@owlint.fr>
2022-06-24 09:57:09 +0200

    feat(notification): component implementation

---
+++ src/components/Notification.stories.js
@@ -0,0 +1,218 @@
+import Notification from './Notification.vue'
+// import Button from './Button.vue'
+import { COLORS } from '../enums/colors'
+import { SIZES } from '../enums/sizes'
+
+export default {
+  title: 'Components/Notification',
+  component: Notification,
+  decorators: [
+    () => ({
+      template: `<div class="flex gap-2 items-center justify-center"><story/></div> `,
+    }),
+  ],
+  argTypes: {
+    color: {
+      control: { type: 'select' },
+      options: COLORS,
+    },
+    size: {
+      control: { type: 'select' },
+      options: SIZES,
+    },
+    hideIcon: {
+      type: { name: 'boolean', required: false },
+    },
+    title: {
+      type: { name: 'string', required: false },
+    },
+    description: {
+      type: { name: 'string', required: false },
+    },
+  },
+}
+
+const defaultArgs = {
+  title: 'Notification title',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perspiciatis doloremque veritatis facilis velit! Corrupti ea ullam enim facere, sed, commodi adipisci ex quos itaque culpa inventore sapiente asperiores consequuntur.',
+}
+
+const Template = (args) => ({
+  components: { Notification },
+  data() {
+    return {
+      displayNotification: false,
+    }
+  },
+  setup() {
+    return { args }
+  },
+  template: `
+            <Notification @close="displayNotification = false" :display-Notification="displayNotification" v-bind="args" >
+             
+            </Notification>`,
+})
+
+export const Base = Template.bind({})
+Base.args = defaultArgs
+
+// export const Icon = (args) => ({
+//   components: { Notification, Button },
+//   data() {
+//     return {
+//       displayNotificationIcon: false,
+//       displayNotification: false,
+//     }
+//   },
+//   setup() {
+//     return { args }
+//   },
+//   template: `<Button @click="displayNotificationIcon = true" label="Dialog Icon" color="fuchsia" />
+//             <Notification @close="displayNotificationIcon = false" :display-Notification="displayNotificationIcon" v-bind="args" >
+//               <template #icon>
+//                 <font-awesome-icon  :icon="['fad', 'burger-cheese']" />
+//               </template>
+//               <template #footer>
+//                 <Button @click="displayNotificationIcon = false" color="fuchsia" label="Close Dialog Icon"/>
+//                 <Button  @click="displayNotificationIcon = false" color="fuchsia" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             <Button @click="displayNotification = true" label="Dialog No Icon" color="fuchsia" />
+//             <Notification @close="displayNotification = false" :display-Notification="displayNotification" v-bind="args" title="Dialog Without Icon" hide-icon   >
+//               <template #footer>
+//                 <Button @click="displayNotification = false" color="fuchsia" label="Close Dialog No Icon"/>
+//                 <Button  @click="displayNotification = false" color="fuchsia" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             `,
+// })
+// Icon.args = {
+//   ...defaultArgs,
+//   title: 'Notification With Icon',
+//   color: 'fuchsia',
+// }
+
+// export const ColorsIcon = (args) => ({
+//   components: { Notification, Button },
+//   data() {
+//     return {
+//       displayNotification: false,
+//       displayNotificationGreen: false,
+//       displayNotificationRed: false,
+//       displayNotificationOrange: false,
+//     }
+//   },
+//   setup() {
+//     return { args }
+//   },
+//   template: `<Button @click="displayNotification = true" label="Dialog"/>
+//             <Notification @close="displayNotification = false" :display-Notification="displayNotification" v-bind="args" >
+//               <template #footer>
+//                 <Button @click="displayNotification = false"  label="Close Dialog"/>
+//                 <Button  @click="displayNotification = false"  type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             <Button @click="displayNotificationGreen = true" label="Dialog Green" color="green" />
+//             <Notification @close="displayNotificationGreen = false" color="green" :display-Notification="displayNotificationGreen" v-bind="args"  >
+//               <template #footer>
+//                 <Button @click="displayNotificationGreen = false" color="green" label="Close Dialog"/>
+//                 <Button  @click="displayNotificationGreen = false" color="green" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             <Button @click="displayNotificationRed = true" label="Dialog Red" color="red" />
+//             <Notification @close="displayNotificationRed = false" color="red" :display-Notification="displayNotificationRed" v-bind="args"  >
+//               <template #footer>
+//                 <Button @click="displayNotificationRed = false" color="red" label="Close Dialog "/>
+//                 <Button  @click="displayNotificationRed = false" color="red" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             <Button @click="displayNotificationOrange = true" label="Dialog Orange" color="orange" />
+//             <Notification @close="displayNotificationOrange = false" color="orange" :display-Notification="displayNotificationOrange" v-bind="args"  >
+//               <template #footer>
+//                 <Button @click="displayNotificationOrange = false" color="orange" label="Close Dialog No Icon"/>
+//                 <Button  @click="displayNotificationOrange = false" color="orange" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             `,
+// })
+// ColorsIcon.args = {
+//   ...defaultArgs,
+// }
+
+// export const Sizes = (args) => ({
+//   components: { Notification, Button },
+//   data() {
+//     return {
+//       displayNotification: false,
+//       displayNotificationXs: false,
+//       displayNotificationXl: false,
+//       displayNotification7xl: false,
+//     }
+//   },
+//   setup() {
+//     return { args }
+//   },
+//   template: `<Button @click="displayNotification = true" label="Dialog"/>
+//             <Notification @close="displayNotification = false" :display-Notification="displayNotification" v-bind="args" >
+//               <template #footer>
+//                 <Button @click="displayNotification = false" label="Close Dialog"/>
+//                 <Button  @click="displayNotification = false" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             <Button @click="displayNotificationXs = true" label="Dialog xs" />
+//             <Notification @close="displayNotificationXs = false" :display-Notification="displayNotificationXs" size="xs" v-bind="args"  >
+//               <template #footer>
+//                 <Button @click="displayNotificationXs = false" label="Close Dialog"/>
+//                 <Button  @click="displayNotificationXs = false" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             <Button @click="displayNotificationXl = true" label="Dialog xl" />
+//             <Notification @close="displayNotificationXl = false" :display-Notification="displayNotificationXl" size="xl" v-bind="args"  >
+//               <template #footer>
+//                 <Button @click="displayNotificationXl = false" label="Close Dialog "/>
+//                 <Button  @click="displayNotificationXl = false" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             <Button @click="displayNotification7xl = true" label="Dialog 7xl" />
+//             <Notification @close="displayNotification7xl = false" :display-Notification="displayNotification7xl" size="7xl" v-bind="args"  >
+//               <template #footer>
+//                 <Button @click="displayNotification7xl = false" label="Close Dialog No Icon"/>
+//                 <Button  @click="displayNotification7xl = false" type="tertiary" label="Close"/>
+//               </template>
+//             </Notification>
+//             `,
+// })
+// Sizes.args = {
+//   ...defaultArgs,
+// }
+// export const Dark = (args) => ({
+//   components: { Notification, Button },
+//   data() {
+//     return {
+//       displayNotification: false,
+//     }
+//   },
+//   setup() {
+//     return { args }
+//   },
+//   template: `<div class="dark">
+//               <Button @click="displayNotification = true" label="Dialog Dark Mode"/>
+//               <Notification class="dark" @close="displayNotification = false" :display-Notification="displayNotification" v-bind="args" >
+//                 <template #footer>
+//                   <Button @click="displayNotification = false" label="Close Dialog"/>
+//                   <Button  @click="displayNotification = false" type="tertiary" label="Close"/>
+//                 </template>
+//               </Notification>
+//             </div>
+//             `,
+// })
+// Dark.args = {
+//   ...defaultArgs,
+// }
+// Dark.parameters = {
+//   backgrounds: {
+//     values: [{ name: 'nosferatu-900', value: '#373a4d' }],
+//     default: 'nosferatu-900',
+//   },
+// }
---
+++ src/components/Notification.vue
@@ -0,0 +1,126 @@
+<template>
+  <div
+    aria-live="assertive"
+    class="flex items-end pointer-events-none sm:items-start"
+  >
+    <div class="w-full flex flex-col items-center space-y-4 sm:items-end">
+      <transition
+        enter-active-class="transform ease-out duration-300 transition"
+        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <div
+          v-if="show"
+          class="
+            max-w-sm
+            w-full
+            bg-white
+            dark:bg-nosferatu-800
+            shadow-lg
+            rounded-lg
+            pointer-events-auto
+            ring-2 ring-black
+            dark:ring-dracula-400
+            ring-opacity-5
+            overflow-hidden
+          "
+        >
+          <div class="p-4">
+            <div class="flex items-start">
+              <div class="flex-shrink-0">
+                <font-awesome-icon
+                  class="w-6 h-6"
+                  :class="style"
+                  :icon="['fad', icon]"
+                ></font-awesome-icon>
+              </div>
+              <div class="ml-3 w-0 flex-1 pt-0.5">
+                <p class="text-sm font-medium text-slate-900 dark:text-white">
+                  {{ title }}
+                </p>
+                <p class="text-xs text-slate-500 dark:text-nosferatu-200">
+                  {{ description }}
+                </p>
+              </div>
+              <div class="ml-4 flex-shrink-0 flex" v-if="isClose">
+                <button
+                  @click="show = false"
+                  class="
+                    bg-white
+                    rounded-md
+                    inline-flex
+                    text-slate-400
+                    hover:text-slate-500
+                    focus:outline-none
+                    focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+                  "
+                >
+                  <span class="sr-only">Close</span>
+                  <font-awesome-icon
+                    class="w-5 h-5"
+                    :icon="['fad', 'xmark']"
+                  ></font-awesome-icon>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(true)
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  icon: {
+    type: String,
+    required: false,
+    default: 'circle-check',
+  },
+  color: {
+    type: String,
+    required: false,
+    default: 'green',
+    validator(value) {
+      return ['orange', 'red', 'green', 'blue'].indexOf(value) !== -1
+    },
+  },
+  isClose: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+})
+
+let style = ''
+
+switch (props.color) {
+  case 'red':
+    style = 'text-red-400 dark:text-marcelin-400'
+    break
+  case 'blue':
+    style = 'text-blue-400 dark:text-dracula-400'
+    break
+  case 'orange':
+    style = 'text-orange-400 dark:text-morbius-400'
+    break
+  case 'green':
+    style = 'text-green-400 dark:text-blade-400'
+    break
+  default:
+    style = 'text-blue-400 dark:text-dracula-400'
+    break
+}
+</script>

f4d38300e20b6550aff87c1a472bf8dbd65fe91e
Lucas <lucas@owlint.fr>
2022-06-29 17:02:45 +0200

    feat(notification-list): component implementation

--- src/components/NotificationList.stories.js
+++ src/components/NotificationList.stories.js
@@ -1,8 +1,6 @@
 import Notification from './Notification.vue'
 import NotificationList from './NotificationList.vue'
 import Button from './Button.vue'
-import { emitter } from '../utils/emmiter'
-import { COLORS } from '../enums/colors'
 import { ref } from 'vue'
 
 export default {
@@ -25,39 +23,20 @@ const Template = () => ({
 
   setup() {
     const notifications = ref([])
-    const notificationCount = ref(0)
-    const removeNotification = (id) => {
-      notifications.value = notifications.value.filter((n) => n.id !== id)
-    }
-    emitter.on('notification', ({ title, description, type, color }) => {
-      const notification = {
-        id: notificationCount.value,
-        type,
-        color,
-        title,
-        description,
-      }
-
-      notificationCount.value += 1
-      notifications.value.push(notification)
-      setTimeout(() => {
-        removeNotification(notification.id)
-      }, 3000)
-    })
     const newNotificationDefault = () => {
-      emitter.emit('notification', {
+      notifications.value.push({
         title: 'Notification Title',
         color: 'green',
         type: 'success',
       })
     }
     const newNotificationNoIcon = () => {
-      emitter.emit('notification', {
+      notifications.value.push({
         title: 'Notification Title',
       })
     }
     const newNotificationDescription = () => {
-      emitter.emit('notification', {
+      notifications.value.push({
         title: 'Notification Title',
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus autem, in dolorum asperiores, dolor esse quo excepturi voluptates sit molestiae incidunt tenetur impedit sed modi soluta suscipit nostrum, porro officia?',

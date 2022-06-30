580b8509ccab7058db4c31378d3a6ab049f24505
Lucas <lucas@owlint.fr>
2022-06-30 13:30:30 +0200

    feat(filter): component implementation

---
+++ src/components/FilterItem.stories.js
@@ -0,0 +1,114 @@
+import FilterItem from './FilterItem.vue'
+import { COLORS } from '../enums/colors'
+
+export default {
+  component: FilterItem,
+  //👇 Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+  title: 'Components/FilterItem',
+  decorators: [() => ({ template: '<div class="flex"><story/></div>' })],
+  argTypes: {
+    color: {
+      options: COLORS,
+      control: { type: 'select' },
+    },
+    value: {
+      type: { name: 'string', required: true },
+    },
+    name: {
+      type: { name: 'string', required: true },
+    },
+    id: {
+      type: { name: 'string', required: true },
+    },
+    modelValue: {
+      type: { name: 'array', required: true },
+    },
+    tile: {
+      type: { name: 'boolean', required: false },
+    },
+  },
+}
+const defaultArgs = {
+  value: 'Burgir',
+  name: 'Burgir',
+  id: 'storybook-id',
+  modelValue: [],
+}
+
+const Template = (args) => ({
+  components: { FilterItem },
+  setup() {
+    return { args }
+  },
+  template: `<FilterItem v-bind="args" >
+            <font-awesome-icon
+                :icon="['fad', 'burger-cheese']"
+            />
+        </FilterItem>`,
+})
+
+export const Default = Template.bind({})
+Default.args = defaultArgs
+
+export const Types = (args) => ({
+  components: { FilterItem },
+  setup() {
+    return { args }
+  },
+  template: `<div class="flex gap-4 flex-wrap">
+              <FilterItem v-bind="args" >
+                  <font-awesome-icon
+                      :icon="['fad', 'burger-cheese']"
+                  />
+              </FilterItem>
+              <FilterItem v-bind="args" >
+                <div class=" flex items-center gap-2">
+                  <font-awesome-icon
+                      :icon="['fad', 'burger-cheese']"
+                  />
+                  <span class="text-xs">With Text</span>
+                </div>
+              </FilterItem>
+              <FilterItem tile v-bind="args" >
+                <div class=" flex items-center gap-2">
+                  <font-awesome-icon
+                      :icon="['fad', 'burger-cheese']"
+                  />
+                  <span class="text-xs">Tile</span>
+                </div>
+              </FilterItem>
+              <FilterItem v-bind="args" :modelValue="['Burgir']">
+                <div class=" flex items-center gap-2">
+                  <font-awesome-icon
+                      :icon="['fad', 'burger-cheese']"
+                  />
+                  <span class="text-xs">Active</span>
+                </div>
+              </FilterItem>
+            </div>`,
+})
+Types.args = defaultArgs
+
+export const Colors = (args) => ({
+  components: { FilterItem },
+  setup() {
+    const colors = COLORS
+    return { args, colors }
+  },
+  template: `<div class="space-y-4">
+              <div v-for="color in colors" :key="color" class="flex gap-4">
+                <FilterItem  :color="color" v-bind="args">
+                  <font-awesome-icon
+                    :icon="['fad', 'burger-cheese']"
+                  />
+                </FilterItem>
+                <FilterItem  :color="color" v-bind="args" :modelValue="['Burgir']">
+                  <font-awesome-icon
+                    :icon="['fad', 'burger-cheese']"
+                  />
+                </FilterItem>
+              </div>
+            </div>`,
+})
+Colors.args = defaultArgs
---
+++ src/components/Filters.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="space-y-4">
+    <h4 v-if="label" class="font-medium dark:text-white text-sm">
+      {{ label }} :
+    </h4>
+    <div class="flex items-center gap-4 ml-2">
+      <FilterItem
+        v-for="item in items.data"
+        :key="item.type"
+        :id="`${items.type}-type-${item.type}`"
+        :name="item.type"
+        :value="item.type"
+        :color="item.color"
+        v-model="modelValue"
+        @input:modelValue="filterChoice($event)"
+      >
+        <font-awesome-icon
+          :icon="[items.type === 'network' ? 'fab' : 'fad', item.icon]"
+        />
+      </FilterItem>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import FilterItem from '../components/FilterItem.vue'
+defineEmits(['update:modelValue'])
+
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    required: true,
+  },
+  items: {
+    type: Object,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+})
+
+const filterChoice = (ev) => {
+  if (ev.checked) {
+    props.modelValue.push(ev.value)
+  } else {
+    const indexValues = props.modelValue.findIndex((v) => v === ev.value)
+    props.modelValue.splice(indexValues, 1)
+  }
+}
+</script>

c6238a284c0508e31871416352100019e14a9ca7
Lucas Gault <lucas@owlint.fr>
2024-01-22 11:04:17 +0100

    new storybook version

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,43 +1,60 @@
 import '../src/style.css'
 
-import { app } from '@storybook/vue3'
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+/** @type { import('@storybook/vue3').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      values: [
+        { name: 'white', value: '#ffffff' },
+        { name: 'slate-100', value: '#f1f5f9' },
+        { name: 'slate-800', value: '#1e293b' },
+        { name: 'slate-900', value: '#0f172a' },
+      ],
+      default: 'white',
     },
   },
-  backgrounds: {
-    values: [
-      { name: 'white', value: '#ffffff' },
-      { name: 'slate-100', value: '#f1f5f9' },
-      { name: 'slate-800', value: '#1e293b' },
-      { name: 'slate-900', value: '#0f172a' },
-    ],
-    default: 'white',
+  globalTypes: {
+    backgrounds: {
+      values: [
+        { name: 'white', value: '#ffffff' },
+        { name: 'slate-100', value: '#f1f5f9' },
+        { name: 'slate-800', value: '#1e293b' },
+        { name: 'slate-900', value: '#0f172a' },
+      ],
+      default: 'white',
+    },
   },
-}
+  decorators: [
+    (story, context) => {
+      const background = context.globals.backgrounds?.value
 
-export const globalTypes = {
-  backgrounds: {
-    values: [
-      { name: 'white', value: '#ffffff' },
-      { name: 'slate-100', value: '#f1f5f9' },
-      { name: 'slate-800', value: '#1e293b' },
-      { name: 'slate-900', value: '#0f172a' },
-    ],
-    default: 'white',
-  },
+      const dark = ['#1e293b', '#0f172a'].includes(background)
+
+      if (dark) {
+        document.querySelector('html').classList.add('of-dark')
+      } else {
+        document.querySelector('html').classList.remove('of-dark')
+      }
+
+      return {
+        template: `<story/>`,
+      }
+    },
+  ],
 }
 
 export const decorators = [
   (story, context) => {
     const background = context.globals.backgrounds?.value
 
-    const dark = background === '#1e293b' || background === '#0f172a'
+    const dark = ['#1e293b', '#0f172a'].includes(background)
 
     if (dark) {
       document.querySelector('html').classList.add('of-dark')
@@ -50,3 +67,5 @@ export const decorators = [
     }
   },
 ]
+
+export default preview
